dd8ad791f1496c4afb856dd4d5ac9771ddc0d082
gaowei.liu <gaowei.liu@ly.com>
2020-08-21 17:36:23 +0800

    v2.0

--- orderbasejar/src/main/java/com/ly/traffic/middleplatform/domain/createorder/repository/ICreateOrderRepository.java
+++ orderbasejar/src/main/java/com/ly/traffic/middleplatform/domain/createorder/repository/ICreateOrderRepository.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liugw
@@ -16,19 +17,19 @@ import java.util.concurrent.ExecutionException;
  * @Description: ${TODO}
  * @date 2020/7/9 10:21
  */
-public interface IOrderRepository {
+public interface ICreateOrderRepository {
     default void save() {
         save(this);
 //        return null;
     }
 
     default String getById(Long id) {
-        RequestContext requestContext = new RequestContext("http://localhost:8096/order/selectOne2");
-        JSONObject body = new JSONObject();
-        body.put("id", id);
+        RequestContext requestContext = new RequestContext("http://localhost:8096/order/selectOne?id=" + id);
+//        JSONObject body = new JSONObject();
+//        body.put("id", id);
 //        System.out.println(body.toJSONString());
-        requestContext.setRequestBody(body.toJSONString());
-        CompletableFuture<String> result = HttpFactory.httpAsyncClientFactory.post(requestContext);
+//        requestContext.setRequestBody(body.toJSONString());
+        CompletableFuture<String> result = HttpFactory.httpAsyncClientFactory.get(requestContext);
 //        result.whenComplete((r,e) -> {
 //            System.out.println("结果：" + r);
 //
@@ -37,23 +38,23 @@ public interface IOrderRepository {
 //            }
 //        });
         try {
-            String s = result.get();
+            String s = result.get(10, TimeUnit.SECONDS);
             if (StringUtils.isEmpty(s)) {
                 return s;
             }
-            JSONObject jsonObject = JSONObject.parseObject(s);
-            JSONObject extendcontent = jsonObject.getJSONObject("extendcontent");
+            JSONObject jsonObject = JSONObject.parseObject(s).getJSONObject("data");
+            JSONObject extendcontent = jsonObject.getJSONObject("extendContent");
             Set<String> keySet = extendcontent.keySet();
             for (String key : keySet) {
                 jsonObject.put(key, extendcontent.get(key));
             }
-            jsonObject.remove("extendcontent");
+            jsonObject.remove("extendContent");
 //            System.out.println("s=" + s);
 //            System.out.println("toJsonStr:" + JSON.toJSONString(jsonObject));
             return JSON.toJSONString(jsonObject);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return "";
