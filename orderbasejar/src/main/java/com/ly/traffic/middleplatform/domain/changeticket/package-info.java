75e7d594ab33ef8d55f20fd46bd1ebd5d8542ed2
gaowei.liu <gaowei.liu@ly.com>
2020-08-27 18:35:38 +0800

    v2.0

---
+++ orderbasejar/src/main/java/com/ly/traffic/middleplatform/domain/changeticket/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Package com.ly.traffic.middleplatform.domain.changeticket
+ * @Description: ${TODO}
+ * @author liugw
+ * @date 2020/8/27 18:09
+ */
+package com.ly.traffic.middleplatform.domain.changeticket;
